7073768eca0115c7ac6ff16758733a3b81be4655
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-25 22:26:48 +0100

    Simple sortable wip

---
+++ src/draggable/sortable.directive.js
@@ -0,0 +1,35 @@
+// @ts-check
+/// <reference lib="dom" />
+(function (angular, undefined) {
+
+
+    function SortableDirective($element) {
+        /** @var {HTMLElement} element */
+        this.element = $element.get(0);
+        /** @var {Sortable} sortable */
+        this.sortable = undefined;
+    }
+    SortableDirective.prototype = {
+
+        $onInit: function () {
+            this.sortable = new window.Draggable.Sortable(this.element, {
+                draggable: '.draggable',
+                mirror: {
+                    appendTo: this.element,
+                    constrainDimensions: true,
+                },
+            });
+        },
+    };
+    SortableDirective.$inject = ['$element'];
+
+    var descriptor = function () {
+        return {
+            restrict: 'A',
+            bindToController: {},
+            controller: SortableDirective,
+        };
+    }
+    angular.module('draggable')
+        .directive('sortable', descriptor);
+})(angular);
